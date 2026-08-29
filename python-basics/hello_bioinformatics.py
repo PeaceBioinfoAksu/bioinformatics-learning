78f811f61ec456e167c3f7ab16827345c161ce62
Peace sunny <theateliers028@gmail.com>
2026-08-29 18:00:55 +0100

    Update hello_bioinformatics.py

--- python-basics/hello_bioinformatics.py
+++ python-basics/hello_bioinformatics.py
@@ -1,2 +1,20 @@
-print("Hello, bioinformatics!")
-print("I am learning Python for biological data analysis.")llo
+# DNA sequence analysis
+
+dna = "ATGCGTACGTTAGC"
+
+print("DNA sequence:", dna)
+print("Sequence length:", len(dna))
+
+a = dna.count("A")
+t = dna.count("T")
+g = dna.count("G")
+c = dna.count("C")
+
+print("A:", a)
+print("T:", t)
+print("G:", g)
+print("C:", c)
+
+gc_content = ((g + c) / len(dna)) * 100
+
+print("GC content:", gc_content, "%")
