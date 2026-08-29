a6b057e09ee03f8c72a164394a50cb0f9936bc35
Peace sunny <theateliers028@gmail.com>
2026-08-29 20:25:46 +0100

    Update fasta_reader.py

--- projects/dna-sequence-analysis/fasta_reader.py
+++ projects/dna-sequence-analysis/fasta_reader.py
@@ -1,4 +1,4 @@
-# Simple FASTA File Reader
+# FASTA Sequence Analyzer
 # Beginner Bioinformatics Project
 
 filename = "example.fasta"
@@ -9,7 +9,29 @@ with open(filename, "r") as file:
 header = lines[0].strip()
 sequence = "".join(line.strip() for line in lines[1:])
 
-print("=== FASTA FILE ===")
+# Base counts
+a = sequence.count("A")
+t = sequence.count("T")
+g = sequence.count("G")
+c = sequence.count("C")
+
+# GC content
+gc_content = ((g + c) / len(sequence)) * 100
+
+# DNA to RNA transcription
+rna = sequence.replace("T", "U")
+
+# Display results
+print("=== FASTA SEQUENCE ANALYSIS ===")
 print("Header:", header)
-print("Sequence:", sequence)
-print("Length:", len(sequence))
+print("DNA sequence:", sequence)
+print("Sequence length:", len(sequence))
+
+print("\nBase counts:")
+print("A:", a)
+print("T:", t)
+print("G:", g)
+print("C:", c)
+
+print("\nGC content:", round(gc_content, 2), "%")
+print("RNA sequence:", rna)
