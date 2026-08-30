8359aa72658837f2996ecea7c321614aca8ce17a
Peace sunny <theateliers028@gmail.com>
2026-08-30 11:10:58 +0100

    Update fasta_reader.py

--- projects/dna-sequence-analysis/fasta_reader.py
+++ projects/dna-sequence-analysis/fasta_reader.py
@@ -1,5 +1,7 @@
-# FASTA Sequence Analyzer
+# ==========================================
+# FASTA SEQUENCE ANALYZER
 # Beginner Bioinformatics Project
+# ==========================================
 
 filename = "example.fasta"
 
@@ -38,6 +40,8 @@ complement = sequence.translate(str.maketrans("ATGC", "TACG"))
 reverse_complement = complement[::-1]
 # Display results
 print("=== FASTA SEQUENCE ANALYSIS ===")
+print("This program analyzes a DNA sequence from a FASTA file.")
+print()
 print("Header:", header)
 print("DNA sequence:", sequence)
 print("Sequence length:", len(sequence))
@@ -71,33 +75,4 @@ print("\nGC content:", round(gc_content, 2), "%")
 print("AT content:", round(at_content, 2), "%")
 print("GC interpretation:", gc_interpretation)
 print("RNA sequence:", rna)
-print("Reverse complement:", reverse_complement);a# Classify sequence length
-if len(sequence) < 20:
-    print("Sequence classification: Short sequence")
-elif len(sequence) < 100:
-    print("Sequence classification: Medium sequence")
-else:
-    print("Sequence classification: Long sequence")
-print("\nBase counts:")
-print("A:", a)
-print("T:", t)
-print("G:", g)
-print("C:", c)
-print("\nBase frequencies:")
-print("A:", round((a / len(sequence)) * 100, 2), "%")
-print("T:", round((t / len(sequence)) * 100, 2), "%")
-print("G:", round((g / len(sequence)) * 100, 2), "%")
-print("C:", round((c / len(sequence)) * 100, 2), "%")
-# Check nucleotide diversity
-unique_bases = len(set(sequence))
-
-print("\nNucleotide diversity:", unique_bases, "different bases")
-
-if unique_bases == 4:
-    print("The sequence contains all four DNA bases.")
-else:
-    print("The sequence does not contain all four DNA bases.")
-print("\nGC content:", round(gc_content, 2), "%")
-print("AT content:", round(at_content, 2), "%")
-print("RNA sequence:", rna)
-print("Reverse complement:", reverse_complement);
+print("Reverse complement:", reverse_complement);a
